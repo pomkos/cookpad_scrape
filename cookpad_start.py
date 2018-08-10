0f0e5a00508ee77eaa5793704528eff3ac749388
pomkos <albeithome@gmail.com>
2018-08-10 15:35:10 -0400

    added new page and redirect via query string

--- cookpad_start.py
+++ cookpad_start.py
@@ -1,4 +1,4 @@
-from flask import request, Flask, render_template
+from flask import request, Flask, render_template, url_for, redirect
 from cookpad_recipe_only import recipe
 import os
 import shutil
@@ -6,23 +6,18 @@ import shutil
 app = Flask(__name__) #create Flask app
 
 @app.route('/', methods=['GET','POST']) #allow get and post requests
-def cookpad_form(): # sending via forms as a post request (behind the scenes)
+def home(): # sending via forms as a post request (behind the scenes)
 
     #--- Check if its a post or get request: ---#
     if request.method == 'POST': #this block is only entered if the form is submitted
-        url = request.form.get('recipe')
+        url = request.form.get('recipe') 
         user = request.form['name']
-        title = recipe(url)
-        publish(user)
-        return render_template('thanks.html', name = user, title = title) # pass the variables to the html page
+        title = recipe(url) #puts the title_ext returned from recipe() into the title variable
+        publish(user) #publishes the scraped recipe into wiki
+        return redirect(url_for('thanks', title=title, user=user)) #redirects to url.com/thanks?title=something&user=something_else. Variables are in the link
 
     #--- Make the form ---#
-    return '''<form method = "POST">
-                    What is the cookpad recipe url? <input type="text" name="recipe"><br>
-                    What is your name? <input type="text" name="name"><br>
-                    <input type="submit" value="Submit"><br>
-                </form>'''
-                # The form makes a post request to the same route that made the form
+    return render_template('home.html')
 
 def publish(user):
     #--- Copy the recipe.doc file to a new dir ---#
@@ -31,13 +26,18 @@ def publish(user):
     os.chdir("/home/peter/NetShare/core")
     os.system("python3 pwb.py login")
     os.system("python3 pwb.py pagefromfile -file:recipe.doc -force -summary:'Uploaded by {}' ".format(user))
-    print('recipe uploaded to wiki')
+
+@app.route('/thanks')
+def thanks():
+    user = request.args.get('user')
+    title = request.args.get('title')
+    return render_template('thanks.html', name = user, title = title) # pass the variables to the html page
 
 @app.route('/test')
 def test():
     user = 'Peter'
     title = 'My Recipe'
-    return render_template('thanks.html', name = user, title = title)
+    return render_template('test.html', name = user, title = title)
 
 
 if __name__ == '__main__':
